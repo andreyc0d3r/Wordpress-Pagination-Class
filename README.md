763b14e3988e2455b98252b1379ba6493b6cec01
Andrey <andreyc0d3r@gmail.com>
2016-11-05 22:25:18 +1100

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Wordpress-Pagination-Class
 This class helps generate pagination by relaying on WordPress core functions. The difference is with this class its easier to modify the styling of the previous/next buttons and page numbers
+
+Pagination class in action:
+https://digitalapps.co/blog/
+
+#Usage
+Include the class
+<?php 
+	include('inc/vendors/class-wordpress-pagination.php');
+?>
+
+Create object of a class.
+Options $args exaplained on the official WordPress website:
+https://codex.wordpress.org/Function_Reference/paginate_links
+<?php 
+   global $wp_query;
+   $args = array(
+        'base'               => str_replace( 2, '%#%', esc_url( get_pagenum_link( 2 ) ) ), //user integer > 1, otherwise pagination returns initial page, which is /blog/
+        'format'             => '/page/%#%/',
+        'total'              => $wp_query->max_num_pages,
+        'current'            => max( 1, get_query_var('paged') ),
+        'show_all'           => false,
+        'end_size'           => 1,
+        'mid_size'           => 2,
+        'prev_next'          => true,
+        'prev_text'          => __('Previous Page'),
+        'next_text'          => __('Next Page'),
+        'type'               => 'array',
+        'add_args'           => false,
+        'add_fragment'       => '',
+        'before_page_number' => '',
+        'after_page_number'  => ''
+    );
+   $_WPP = new WPP_Class($args);
+?>
---
+++ class-wordpress-pagination.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * Digital Apps - Wordpress Pagination Class
+ *
+ * (c) 2016 Andrey M. / DigitalApps.co
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! class_exists( 'WPP_Class' ) ) {
+
+	class WPP_Class {
+
+		/**
+		 * Init Variables
+		 */
+	    public $settings = array(
+							'items_wrapper_open'		=> '<ul>',
+							'items_wrapper_close'		=> '</ul>',
+							'item_open'					=> '<li>',
+							'item_close'				=> '</li>',
+						);
+
+		/**
+		 * Constructor
+		 */
+
+		public function __construct( $args ) {
+			$this->args = $args;
+			$this->get_pagination();
+		}
+
+		/**
+		 * Public functions
+		 */
+
+		public function get_pagination() {
+			if(!empty($this->args)) :
+				$links_set = paginate_links( $this->args );
+				$array_total = count($links_set) - 1;
+				$current_page_count = 1;
+				$pagination_html = '';
+				$prev_next_buttons_html = '';
+
+				foreach ($links_set as $key => $value) {
+					// set previous link
+					if( $key == 0 && $this->args['current'] > 1 ) {
+						$prev_next_buttons_html .= $value;
+						$current_page_count--;
+					}
+					// set next link
+					if( $key != $this->args['current'] && $key == $array_total ) {
+						$prev_next_buttons_html .= $value;
+					}
+					// set page numbers
+					if( $this->args['current'] == $current_page_count ) {
+						// set current page
+						$pagination_html .= $this->settings['item_open'] . '<a class="active" href="#">' . $value . '</a>' . $this->settings['item_close'];
+					} else {
+						// set all other pages
+						if( $key != 0 && $key != $array_total ) {
+							$pagination_html .= $this->settings['item_open'] . $value . $this->settings['item_close'];
+						}
+					}
+					$current_page_count++;
+				}
+
+				echo '<div class="paginator wow fadeInUp">';
+					echo $this->settings['items_wrapper_open'] . $pagination_html . $this->settings['items_wrapper_close'];
+					echo '<div>';
+						echo $prev_next_buttons_html;
+					echo '</div>';
+					echo '<div class="clear"></div>';
+				echo '</div>';
+			endif;
+		}
+	}
+}
